5e476a6875bd8383fd07bbb81b9045cfa1394022
Enej Bajgoric <enej.bajgoric@gmail.com>
2012-09-11 09:53:21 -0700

    updates

--- js/importer.js
+++ js/importer.js
@@ -4,7 +4,9 @@ var Post_Importer = {
 	
 		jQuery(".categories").data("placeholder","Select A Category..").chosen();
 		
-		jQuery('.post-meta').append('<a href="#" class="submitdelete deletion">remove</a>');
+		jQuery('.post-meta').append('<a href="#" class="submitdelete deletion">remove</a>').each(function(index, value){
+			jQuery(this).append('<input type="hidden" name="post" />')
+		});
 		
 		jQuery('.submitdelete').on('click', function() {
 			var ok = confirm('are you sure you want to remove the post?')
